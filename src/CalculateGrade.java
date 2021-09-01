ba07cc396e15074192afbcb6aad46705be66351c
klotza5678 <klotza5678@gmail.com>
2021-09-01 18:50:45 +0700

    Change calculate add C+andD+

--- src/CalculateGrade.java
+++ src/CalculateGrade.java
@@ -21,8 +21,14 @@ public class CalculateGrade {
         else if (score >= 70){
             grade = "B";
         }
+        else if (score >= 65){
+            grade = "C+";
+        }
         else if (score >= 60){
             grade = "C";
+        }
+        else if (score >= 55){
+            grade = "D+";
         }
         else if (score >= 50){
             grade = "D";
